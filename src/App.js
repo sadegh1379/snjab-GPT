182b86e3eff3dfaaa8db98fff17fcfef82d35b9f
sadegh <akbarisadegh382@gmail.com>
2023-08-14 01:39:15 +0330

    fixed public manifest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+// import axios from "axios";
 import ChatView from "./components/ChatView";
 import Header from "./components/Header";
 import { ChatContextProvider } from "./context/chatContext";
+// import { useEffect } from "react";
 
 const App = () => {
+
+  // const getData = async () => {
+  //   const res = await axios.get("https://api.ipify.org/?format=json");
+  //   console.log(res.data);
+  //   // setIP(res.data.ip);
+  // };
+
+  // useEffect(() => {getData()}, [])
+
   return (
     <ChatContextProvider>
       <div className="main">
