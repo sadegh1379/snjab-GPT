c7c6bd494a311a05797a92a61496ec5794612a14
sadegh <akbarisadegh382@gmail.com>
2023-08-19 16:05:36 +0330

    fix

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@
 import ChatView from "./components/ChatView";
 import Header from "./components/Header";
 import { ChatContextProvider } from "./context/chatContext";
-// import { useEffect } from "react";
 
 const App = () => {
-
-  // const getData = async () => {
-  //   const res = await axios.get("https://api.ipify.org/?format=json");
-  //   console.log(res.data);
-  //   // setIP(res.data.ip);
-  // };
-
-  // useEffect(() => {getData()}, [])
 
   return (
     <ChatContextProvider>
--- src/utils/davinci.js
+++ src/utils/davinci.js
@@ -3,19 +3,18 @@ import { Configuration, OpenAIApi } from 'openai';
 export const davinci = async (prompt, key) => {
   const configuration = new Configuration({
     // apiKey: process.env.REACT_APP_OPEN_AI_API_KEY,
-    apiKey: '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',
-    basePath: 'https://aisnjob.com',
+    apiKey: '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',
+    // basePath: 'https://aisnjob.com',
   });
 
   const openai = new OpenAIApi(configuration);
-
+ 
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
       {
-        role: 'system',
-        content:
-          "you're an a AI assistant that replies to all my questions in markdown format.",
+        role: "system",
+        content: "You are ChatGPT. Respond to the user like you normally would.",
       },
       { role: 'user', content: 'hi' },
       { role: 'assistant', content: 'Hi! How can I help you?' },
@@ -23,10 +22,6 @@ export const davinci = async (prompt, key) => {
     ],
     temperature: 0.3,
     max_tokens: 1000,
-    top_p: 0.3,
-    frequency_penalty: 0.5,
-    presence_penalty: 0.2,
-    
   });
 
   return response;
